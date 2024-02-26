2e99dc726822ad20541b87357702e98f48ff3e9a
D1f3r3nt <98466831+D1f3r3nt@users.noreply.github.com>
2024-02-26 13:07:49 +0100

    Add experience

--- src/pages/ExperiencePage.tsx
+++ src/pages/ExperiencePage.tsx
@@ -1,11 +1,37 @@
 import {Page} from "../components/organisms/Page.tsx";
 import {Text} from "../components/atoms/Text.tsx";
+import {useTranslation} from "react-i18next";
+import {CenterCard} from "../components/molecules/CenterCard.tsx";
+
+interface ExperienceInterface {
+    title: string;
+    date: string;
+    description: string;
+}
 
 export const ExperiencePage = () => {
+    const [t] = useTranslation("global")
+
+    const experiences = t('experience.data', { returnObjects: true }) as unknown as ExperienceInterface[]
 
     return (
         <Page>
-            <Text value={'ExperiencePage'} size={'xl'} />
+            <Text value={t('header.experience')} size={'xl'} gradient/>
+
+            <div className='w-[100%] flex flex-col items-center gap-y-5'>
+                {
+                    experiences.reverse().map((degree, i) => {
+                        return (
+                            <>
+                                <CenterCard title={degree.title} date={degree.date} description={degree.description} />
+                                { i !== experiences.length - 1 &&
+                                    <hr className='max-w-[850px] line-gradient'/>
+                                }
+                            </>
+                        )
+                    })
+                }
+            </div>
         </Page>
     )
 }
