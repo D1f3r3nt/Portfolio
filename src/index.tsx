c78f8212943b6e2149662bdbebc1a6c275b6e381
D1f3r3nt <98466831+D1f3r3nt@users.noreply.github.com>
2024-11-26 15:31:44 +0100

    fix: Google Analytics

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-3XQHH4FE5L")
+
+ReactGA.send({
+    hitType: "pageview",
+    page: "/",
+    title: "Home Portfolio"
+})
+
+ReactGA.send({
+    hitType: "pageview",
+    page: "/projects",
+    title: "Projects Page"
+})
+
+ReactGA.send({
+    hitType: "pageview",
+    page: "/certificates",
+    title: "Certificates Page"
+})
+
+ReactGA.send({
+    hitType: "pageview",
+    page: "/brain",
+    title: "Mind Map"
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
